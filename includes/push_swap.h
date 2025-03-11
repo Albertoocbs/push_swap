875a2ecacdc41fe79f3dbc4b904b93fa84098465
Alberto Outumuro Bueno <albertooutumurobueno@gmail.com>
2025-03-11 11:30:09 +0100

    Add files via upload

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: aoutumur <aoutumur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 10:52:10 by aoutumur          #+#    #+#             */
-/*   Updated: 2025/03/07 11:54:11 by aoutumur         ###   ########.fr       */
+/*   Updated: 2025/03/10 17:36:33 by aoutumur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,11 @@
 # define PUSH_SWAP_H
 
 # include "ft_printf/ft_printf.h"
+# include <stddef.h>
 # include <stdlib.h>
 # include <unistd.h>
 # include <limits.h>
+# include <stdio.h>
 
 /*structure globale qui regroupe les deux piles(stacks) et leur taille*/
 typedef struct s_swap
@@ -34,6 +36,7 @@ int	is_correct_input(char **args);
 t_list	*ft_new_list_node(int valeur);
 void	ft_add_node_back(t_list **stack, t_list *new_node);
 t_list	*fill_stack(char **args);
+void	assign_index(t_list *stack);
 
 /*------------Swap Functions---------*/
 void	swap_sa(t_list **stack_a);
@@ -58,15 +61,18 @@ void	rev_rotate_rrr(t_list **stack_a, t_list **stack_b);
 /*--------Utils(Errors & MemFree) Functions-------*/
 void	free_args(char **args);
 void	free_stack(t_list *stack);
+void	free_all_stacks(t_swap *tab);
 void	exit_error(t_swap *tab, char **args);
+void	check_and_exit(t_swap *tab, char **args, int argc);
 
 /*-----------Sort Handler Functions-----------*/
 int	check_sorted(t_list *stack);
-void	sort_3nbr(t_swap *tab);
-void	sort_5nbr(t_swap *tab);
+void	sort_3nbr(t_list **stack);
+void	sort_5nbr(t_list **stack_a, t_list **stack_b);
 int	ft_list_size(t_list *stack);
 
-/*-----------Sorting Algorithms Push_swap.c-----------*/
+/*-----------Sorting Algorithms sort.c-----------*/
 void	sort_stack(t_swap *tab);
+void	radix_sort(t_list **stack_a, t_list **stack_b);
 
 #endif
