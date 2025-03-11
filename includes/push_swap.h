a2e5769c6fae432ea92276aedb28249fc18a869d
Alberto Outumuro Bueno <albertooutumurobueno@gmail.com>
2025-03-11 11:36:42 +0100

    Add files via upload

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: aoutumur <aoutumur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 10:52:10 by aoutumur          #+#    #+#             */
-/*   Updated: 2025/03/10 17:36:33 by aoutumur         ###   ########.fr       */
+/*   Updated: 2025/03/11 11:36:11 by aoutumur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,12 +30,12 @@ typedef struct s_swap
 }	t_swap;
 
 /*-------------Input_Check-------------*/
-int	is_correct_input(char **args);
+int		is_correct_input(char **args);
 
 /*----------Stack Initialisation -----*/
 t_list	*ft_new_list_node(int valeur);
-void	ft_add_node_back(t_list **stack, t_list *new_node);
 t_list	*fill_stack(char **args);
+void	ft_add_node_back(t_list **stack, t_list *new_node);
 void	assign_index(t_list *stack);
 
 /*------------Swap Functions---------*/
@@ -57,7 +57,6 @@ void	rev_rotate_rra(t_list **stack_a);
 void	rev_rotate_rrb(t_list **stack_b);
 void	rev_rotate_rrr(t_list **stack_a, t_list **stack_b);
 
-
 /*--------Utils(Errors & MemFree) Functions-------*/
 void	free_args(char **args);
 void	free_stack(t_list *stack);
@@ -66,10 +65,10 @@ void	exit_error(t_swap *tab, char **args);
 void	check_and_exit(t_swap *tab, char **args, int argc);
 
 /*-----------Sort Handler Functions-----------*/
-int	check_sorted(t_list *stack);
+int		check_sorted(t_list *stack);
 void	sort_3nbr(t_list **stack);
 void	sort_5nbr(t_list **stack_a, t_list **stack_b);
-int	ft_list_size(t_list *stack);
+int		ft_list_size(t_list *stack);
 
 /*-----------Sorting Algorithms sort.c-----------*/
 void	sort_stack(t_swap *tab);
