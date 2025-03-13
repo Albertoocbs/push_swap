9a9f6cda04c0757463426d1a0573ee21a4f23585
Alberto Outumuro Bueno <albertooutumurobueno@gmail.com>
2025-03-13 14:19:01 +0100

    Add files via upload

--- ft_printf/libft/ft_atol.c
+++ ft_printf/libft/ft_atol.c
@@ -6,34 +6,36 @@
 /*   By: aoutumur <aoutumur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 12:22:53 by aoutumur          #+#    #+#             */
-/*   Updated: 2025/03/04 12:24:35 by aoutumur         ###   ########.fr       */
+/*   Updated: 2025/03/13 13:56:30 by aoutumur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <limits.h>
 
-long ft_atol(const char *s)
+long	ft_atol(const char *s)
 {
-	long result;
-	int sign;
-	int i;
+	long	result;
+	int		sign;
+	int		i;
 
 	result = 0;
 	sign = 1;
 	i = 0;
 	while (s[i] == ' ' || (s[i] >= 9 && s[i] <= 13))
-	i++;
-
+		i++;
 	if (s[i] == '-' || s[i] == '+')
 	{
 		if (s[i] == '-')
-		sign = -1;
+			sign = -1;
 		i++;
 	}
-	while (s[i])
+	while (s[i] >= '0' && s[i] <= '9')
 	{
 		result = result * 10 + (s[i] - '0');
 		i++;
+		if ((result * sign) > INT_MAX || (result * sign) < INT_MIN)
+			return (LONG_MAX);
 	}
 	return (result * sign);
 }
